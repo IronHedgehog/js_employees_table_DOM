f11e381badf244fecbaae9f945bb8337701d06de
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-05-11 17:59:17 +0300

    finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,169 @@
 'use strict';
-
+// 1. Implement table sorting by clicking on the title (in two directions).
 // write code here
+
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+
+let counter = 0;
+let previusTHindex = null;
+
+tHead.addEventListener('click', (e) => {
+  if (e.target.nodeName !== 'TH') {
+    return null;
+  }
+
+  counter++;
+
+  const th = e.target;
+  const columnIndex = th.cellIndex;
+
+  if (previusTHindex !== columnIndex) {
+    previusTHindex = columnIndex;
+    counter = 1;
+  }
+
+  const rowsToSort = Array.from(tBody.rows);
+
+  rowsToSort.sort((a, b) => {
+    const contentA = a.cells[columnIndex].textContent.trim();
+    const contentB = b.cells[columnIndex].textContent.trim();
+
+    const maybeNum = parseFloat(contentA.replace(/[^0-9.-]+/g, ''));
+    const maybeNum2 = parseFloat(contentB.replace(/[^0-9.-]+/g, ''));
+
+    if (!isNaN(maybeNum) && !isNaN(maybeNum2)) {
+      if (counter % 2 === 0) {
+        return maybeNum2 - maybeNum;
+      } else {
+        return maybeNum - maybeNum2;
+      }
+    }
+
+    if (counter % 2 === 0) {
+      return contentA.localeCompare(contentB);
+    } else {
+      return contentB.localeCompare(contentA);
+    }
+  });
+
+  tBody.append(...rowsToSort);
+});
+
+let previusTR = null;
+
+tBody.addEventListener('click', (e) => {
+  const clickedTR = e.target.closest('tr');
+
+  if (previusTR) {
+    previusTR.classList.remove('active');
+  }
+
+  clickedTR.classList.add('active');
+
+  previusTR = clickedTR;
+});
+
+function addForm() {
+  const formHTML = `
+  <form class="new-employee-form">
+    <label>Name: <input name="name" type="text" data-qa="name" required></label>
+    <label>Position: <input name="position" type="text" data-qa="position" required></label>
+    <label>Office:
+      <select name="office" data-qa="office" required>
+        <option value="Tokyo">Tokyo</option>
+        <option value="Singapore">Singapore</option>
+        <option value="London">London</option>
+        <option value="New York">New York</option>
+        <option value="Edinburgh">Edinburgh</option>
+        <option value="San Francisco">San Francisco</option>
+      </select>
+    </label>
+    <label>Age: <input name="age" type="number" data-qa="age" required></label>
+    <label>Salary: <input name="salary" type="number" data-qa="salary" required></label>
+    <button type="submit">Add Employee</button>
+  </form>
+`;
+
+  document.body.insertAdjacentHTML('beforeend', formHTML);
+}
+
+addForm();
+
+const form = document.querySelector('.new-employee-form');
+
+const handleSuccess = (message) => {
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+
+  h2.textContent = message;
+  h2.classList.add('title');
+  div.appendChild(h2);
+
+  div.setAttribute('data-qa', 'notification');
+  div.classList.add('notification', `success`);
+
+  document.body.appendChild(div);
+};
+
+const handleError = (message) => {
+  const div = document.createElement('div');
+  const h2 = document.createElement('h2');
+
+  h2.textContent = message;
+  h2.classList.add('title');
+  div.appendChild(h2);
+  div.setAttribute('data-qa', 'notification');
+  div.classList.add('notification', `error`);
+
+  document.body.appendChild(div);
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.currentTarget);
+
+  const employeeData = Object.fromEntries(formData.entries());
+
+  const salary = Number(employeeData.salary);
+  const salaryToSave = '$' + salary.toLocaleString('en-US');
+
+  if (employeeData.name.length < 4) {
+    handleError('Print name more than four letters');
+
+    return null;
+  }
+
+  if (employeeData.age < 18 || employeeData.age > 90) {
+    handleError('Print new age');
+
+    return null;
+  }
+
+  const dataToSave = {
+    name: employeeData.name,
+    position: employeeData.position,
+    office: employeeData.office,
+    age: employeeData.age,
+    salary: salaryToSave,
+  };
+
+  addEmployeeRow(dataToSave);
+  handleSuccess('added new employee');
+  form.reset();
+});
+
+function addEmployeeRow(employeeData) {
+  if (!tBody) {
+    return;
+  }
+
+  const row = tBody.insertRow();
+
+  row.insertCell().textContent = employeeData.name;
+  row.insertCell().textContent = employeeData.position;
+  row.insertCell().textContent = employeeData.office;
+  row.insertCell().textContent = employeeData.age;
+  row.insertCell().textContent = employeeData.salary;
+}
